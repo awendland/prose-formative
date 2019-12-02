0133eadb6402e2f95841126dcd5c1d0c09741030
Alex Wendland <me@alexwendland.com>
2019-12-01 22:33:45 -0500

    formative: Remove negative AbsPos implementation and replace w/ guidance comments

--- ProseTutorial/synthesis/WitnessFunctions.cs
+++ ProseTutorial/synthesis/WitnessFunctions.cs
@@ -62,19 +62,31 @@ namespace ProseTutorial {
             return new ExampleSpec(result);
         }
 
+        /// <summary>
+        ///     This witness function should deduce the spec for k given the spec for AbsPos
+        /// </summary>
+        /// <param name="rule"></param>
+        /// <param name="spec"></param>
+        /// <returns>
+        ///     However, now we need to produce two possible specs for k (positive and negative)
+        ///     given a single spec for AbsPos. A disjunction of possible specs has its own
+        ///     representative spec type in PROSE – DisjunctiveExamplesSpec.
+        /// </returns>
         [WitnessFunction(nameof(Semantics.AbsPos), 1)]
-        public DisjunctiveExamplesSpec WitnessK(GrammarRule rule, DisjunctiveExamplesSpec spec) {
-
+        public DisjunctiveExamplesSpec WitnessK(GrammarRule rule, DisjunctiveExamplesSpec spec)
+        {
             var kExamples = new Dictionary<State, IEnumerable<object>>();
-            foreach (var example in spec.DisjunctiveExamples) {
+            foreach (KeyValuePair<State, IEnumerable<object>> example in spec.DisjunctiveExamples)
+            {
                 State inputState = example.Key;
                 var v = inputState[rule.Body[0]] as string;
 
                 var positions = new List<int>();
-                foreach (int pos in example.Value) {
-                    positions.Add((int)pos + 1);
-                    positions.Add((int)pos - v.Length - 1);
-                }
+                foreach (int pos in example.Value) //the positive spec for k
+                    positions.Add(pos + 1);
+                // TODO add the negative spec for k 
+                // uncomment the next statement and replace X by the expression that should return the negative spec
+                // positions.Add(X);
                 if (positions.Count == 0) return null;
                 kExamples[inputState] = positions.Cast<object>();
             }
