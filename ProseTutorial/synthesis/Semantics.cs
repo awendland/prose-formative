325d095da7e2372cf54809c56aadc38d57f62e5f
Alex Wendland <me@alexwendland.com>
2019-12-04 01:40:40 -0500

    WIP add Append operator to Substring grammar

--- ProseTutorial/synthesis/Semantics.cs
+++ ProseTutorial/synthesis/Semantics.cs
@@ -9,6 +9,7 @@ using Microsoft.ProgramSynthesis.Utils;
 
 namespace ProseTutorial {
     public static class Semantics {
+        public static string Append(string prefix, string suffix) => prefix + suffix;
         public static string Substring(string v, int start, int end) => v.Substring(start, end - start);
 
         public static int? AbsPos(string v, int k) {
--- ProseTutorial/synthesis/WitnessFunctions.cs
+++ ProseTutorial/synthesis/WitnessFunctions.cs
@@ -22,6 +22,33 @@ namespace ProseTutorial {
     new Regex(@"$")  // End of line
 };
 
+        [WitnessFunction(nameof(Semantics.Append), 0)]
+        public DisjunctiveExamplesSpec WitnessPrefix(GrammarRule rule, ExampleSpec spec) {
+            var result = new Dictionary<State, IEnumerable<object>>();
+
+            foreach (var example in spec.Examples) {
+                State inputState = example.Key;
+                var output = example.Value as string;
+                var substrings = new List<string>();
+                for (int i = 1; i <= output.Length; ++i) {
+                    substrings.Add(output.Substring(0, i));
+                }
+                result[inputState] = substrings.Cast<object>();
+            }
+            return new DisjunctiveExamplesSpec(result);
+        }
+
+        [WitnessFunction(nameof(Semantics.Append), 1, DependsOnParameters = new []{0})]
+        public ExampleSpec WitnessSuffix(GrammarRule rule, ExampleSpec spec, ExampleSpec prefixSpec) {
+            var result = new Dictionary<State, object>();
+            foreach (var example in spec.Examples) {
+                State inputState = example.Key;
+                var output = example.Value as string;
+                var prefix = (string) prefixSpec.Examples[inputState];
+                result[inputState] = output.Substring(prefix.Length);
+            }
+            return new ExampleSpec(result);
+        }
 
         [WitnessFunction(nameof(Semantics.Substring), 1)]
         public DisjunctiveExamplesSpec WitnessStartPosition(GrammarRule rule, ExampleSpec spec) {
