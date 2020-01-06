fb83cd696ebd5ea7fca1915ba75bb2e51a22364a
Nada Amin <namin@alum.mit.edu>
2020-01-07 00:42:20 +0100

    add test case

--- ProseTutorial.Tests/SubstringTest.cs
+++ ProseTutorial.Tests/SubstringTest.cs
@@ -160,6 +160,50 @@ namespace ProseTutorial {
             Assert.AreEqual("Gulwani", output2);
         }
 
+        [TestMethod]
+        public void TestLearnAppendSuffix()
+        {
+            var grammar = CompileGrammar();
+            var prose = ConfigureSynthesis(grammar.Value);
+
+            var input = State.CreateForExecution(grammar.Value.InputSymbol, "Bob");
+            var examples = new Dictionary<State, object> { { input, "BobBo" } };
+
+            var spec = new ExampleSpec(examples);
+
+            var scoreFeature = new RankingScore(grammar.Value);
+            var topPrograms = prose.LearnGrammarTopK(spec, scoreFeature, 1, null);
+            var topProgram = topPrograms.RealizedPrograms.First();
+            var output = topProgram.Invoke(input) as string;
+            Assert.AreEqual("BobBo", output);
+
+            var input2 = State.CreateForExecution(grammar.Value.InputSymbol, "Cat");
+            var output2 = topProgram.Invoke(input2) as string;
+            Assert.AreEqual("CatCa", output2);
+        }
+
+        [TestMethod]
+        public void TestLearnAppendPrefix()
+        {
+            var grammar = CompileGrammar();
+            var prose = ConfigureSynthesis(grammar.Value);
+
+            var input = State.CreateForExecution(grammar.Value.InputSymbol, "Bob");
+            var examples = new Dictionary<State, object> { { input, "BoBob" } };
+
+            var spec = new ExampleSpec(examples);
+
+            var scoreFeature = new RankingScore(grammar.Value);
+            var topPrograms = prose.LearnGrammarTopK(spec, scoreFeature, 1, null);
+            var topProgram = topPrograms.RealizedPrograms.First();
+            var output = topProgram.Invoke(input) as string;
+            Assert.AreEqual("BoBob", output);
+
+            var input2 = State.CreateForExecution(grammar.Value.InputSymbol, "Cat");
+            var output2 = topProgram.Invoke(input2) as string;
+            Assert.AreEqual("CaCat", output2);
+        }
+
         public static SynthesisEngine ConfigureSynthesis(Grammar grammar) {
             var witnessFunctions = new WitnessFunctions(grammar);
             var deductiveSynthesis = new DeductiveSynthesis(witnessFunctions);
